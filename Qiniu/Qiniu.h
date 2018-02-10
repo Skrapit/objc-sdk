17f0dc1e10f58c9a913d0337d5e992ad03f6775a
Wallace Wu <wallace@skrapit.co>
2018-02-09 23:07:34 -0500

    add qiniu framework target

---
+++ Qiniu/Qiniu.h
@@ -0,0 +1,19 @@
+//
+//  Qiniu.h
+//  Qiniu
+//
+//  Created by Zhu Wu on 2018-02-09.
+//  Copyright © 2018 Qiniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Qiniu.
+FOUNDATION_EXPORT double QiniuVersionNumber;
+
+//! Project version string for Qiniu.
+FOUNDATION_EXPORT const unsigned char QiniuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Qiniu/PublicHeader.h>
+
+
